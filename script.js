87c307489e2452f6f8c8fdf6d154eaf39fbf546c
José Eduardo Ormeño Meneses <74284944+eduardoorm@users.noreply.github.com>
2025-07-24 11:31:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Script to show "hola mundo"
 console.log("hola mundo");
 
-// Alternative way to show "hola mundo" in an alert
-// alert("hola mundo");
-
 // Function to display "hola mundo" 
 function mostrarHolaMundo() {
     console.log("hola mundo");
